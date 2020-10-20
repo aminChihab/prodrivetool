048cfb11ecf51114847d6774184cf6404bfcd95b
Amin Chihab <aminchihab@github.com>
2020-10-20 11:17:39 +0200

    hide auth token

--- Models/Auth.cs
+++ Models/Auth.cs
@@ -7,7 +7,7 @@ namespace postersopmetaal.Models
 {
     public class Auth
     {
-        internal static readonly string BASE_64_TOKEN = "PHRva2VuPjx2ZXJzaW9uPjE8L3ZlcnNpb24+PGRhdGE+Mjc0ODNCQTFDNkE0NDM5N0IxRDM2QUY3N0JFQjA5RkE2NkQ4NkQ4MDQ2OTdERUQ4NTRDMUQ2QUE3NEVCMjM4RDwvZGF0YT48L3Rva2VuPg==";
+        internal static readonly string BASE_64_TOKEN = Environment.GetEnvironmentVariable("BASE64_AFAS_AUTH");
 
                
     }
